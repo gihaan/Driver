77b8e3916b7aec571d20d3f8e1c1daacf8a46efb
Gihan <gemy6987@gmail.com>
2018-03-03 22:09:10 -0800

    MyCommitMessage

---
+++ app/src/main/java/com/example/gihan/driver/ui/bb.java
@@ -0,0 +1,15 @@
+package com.example.gihan.driver.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gihan.driver.R;
+
+public class bb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bb);
+    }
+}
